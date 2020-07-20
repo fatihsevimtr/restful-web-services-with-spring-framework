ba21924945f363c2f75175ce1ece4341ad7dd4ba
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-20 17:37:58 +0200

    Post Method Application with Java Object and UUID

--- mobile-app-ws/src/main/java/com/exerate/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/exerate/ui/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.exerate.ui.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,26 +10,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exerate.ui.model.UserRest;
+import com.exerate.ui.model.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	
+
 	@GetMapping
-	public String getUser(@RequestParam(value = "limit", defaultValue = "10") int l, @RequestParam(value = "page",defaultValue = "1") int p,
+	public String getUser(@RequestParam(value = "limit", defaultValue = "10") int l,
+			@RequestParam(value = "page", defaultValue = "1") int p,
 			@RequestParam(value = "sort", defaultValue = "desc", required = false) String s) {
 
-		return "get user method was called with page "+p+" and limit in a page is "+l+" sorted in "+s;
+		return "get user method was called with page " + p + " and limit in a page is " + l + " sorted in " + s;
 	}
 
-	@GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+	@GetMapping(path = "/{userId}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserRest> getUser(@PathVariable("userId") String id) {
-		
+
 		UserRest user = new UserRest();
 		user.setFirstName("Fatih");
 		user.setLastName("Sevim");
@@ -36,10 +41,19 @@ public class UserController {
 		return new ResponseEntity<UserRest>(user, HttpStatus.ACCEPTED);
 	}
 
-	@PostMapping
-	public String createUser() {
+	@PostMapping(consumes = { MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, 
+				produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetailsRequestModel) {
 
-		return "create user method was called";
+		UserRest user = new UserRest();
+		user.setFirstName(userDetailsRequestModel.getFirstName());
+		user.setLastName(userDetailsRequestModel.getLastName());
+		user.setEmail(userDetailsRequestModel.getEmail());
+		user.setUserId(UUID.randomUUID().toString());
+
+		return new ResponseEntity<UserRest>(user, HttpStatus.OK);
+		
+	
 	}
 
 	@PutMapping
@@ -47,7 +61,7 @@ public class UserController {
 
 		return "update user method was called";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 
