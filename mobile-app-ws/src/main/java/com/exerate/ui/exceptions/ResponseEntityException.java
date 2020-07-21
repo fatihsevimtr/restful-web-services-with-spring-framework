b6bd4531f9a735c1e178536f316860f6f9516290
Fatih-Sevim <fatihsevimtr@gmail.com>
2020-07-21 17:07:39 +0200

    Error message customization

--- mobile-app-ws/src/main/java/com/exerate/ui/exceptions/ResponseEntityException.java
+++ mobile-app-ws/src/main/java/com/exerate/ui/exceptions/ResponseEntityException.java
@@ -1,5 +1,7 @@
 package com.exerate.ui.exceptions;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +10,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.exerate.ui.model.response.ErrorMessage;
+
 @ControllerAdvice
 public class ResponseEntityException extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(value = {Exception.class})
 	public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
 		
-		return new ResponseEntity<>(ex,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		String errorMessageDescription= ex.getLocalizedMessage();
+		
+		if(errorMessageDescription==null) errorMessageDescription=ex.toString();
+		
+		ErrorMessage message = new ErrorMessage(new Date(), ex.getLocalizedMessage());
+		
+		return new ResponseEntity<>(message,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
--- mobile-app-ws/src/main/java/com/exerate/ui/model/response/ErrorMessage.java
+++ mobile-app-ws/src/main/java/com/exerate/ui/model/response/ErrorMessage.java
@@ -5,21 +5,30 @@ import java.util.Date;
 public class ErrorMessage {
 	private Date timestamp;
 	private String message;
-	
+
+	public ErrorMessage() {
+
+	}
+
+	public ErrorMessage(Date timestamp, String message) {
+		this.timestamp = timestamp;
+		this.message = message;
+	}
+
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
-	
+
 	public Date getTimestamp() {
 		return timestamp;
 	}
-	
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
+
 	public String getMessage() {
 		return message;
 	}
-	
+
 }
